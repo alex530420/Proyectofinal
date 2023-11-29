9636ffbf642aa92d9ca35cc23e8828dbc53b4f9c
GustavoResendizCast <124696432+GustavoResendizCast@users.noreply.github.com>
2023-11-29 22:56:48 +0000

    Feat correcciones

--- server/app.js
+++ server/app.js
@@ -3,29 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var debug = require('debug')('pila2:server');
+var debug = require('debug')('dwpc2:server');
 
 var indexRouter = require('./routes/index');
-//activa "userroutes " cuando se solicita el recurso raiz
 var usersRouter = require('./routes/users');
 
+// Se crea la instancia de express
 var app = express();
 
-// view engine setup
-// debug(`👻Ruta de app ${__dirname}`);
+// Configurando el motor de plantilla
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// Se establecen los middlwares
+// Se establecen los middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// Crea un server de archivos estaticos
-app.use(express.static(path.join(__dirname,' ..', 'public')));
+// Se crea un server de archivos estaticos 
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
+// Activa "userRouter" cuando se 
+// solicita "/user"
 app.use('/users', usersRouter);
+// app.use('/author', (req, res) => {
+//   res.json({mainDeveloper: "Vianney Reyes"})
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
