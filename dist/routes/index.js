d2566d74eada930b9d0ce93f386a5d9e5df82ed4
alex530420 <124646518+alex530420@users.noreply.github.com>
2023-12-01 21:50:58 -0600

    dist

---
+++ dist/routes/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'ITGAM',
+    author: 'Alejandro Carreon',
+  });
+});
+module.exports = router;
